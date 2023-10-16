f3490defac30df9702d1d2bb631bce96f6dc81d0
Ali Sadri <asadri721@gmail.com>
2023-10-16 11:46:21 +0330

    Add answers module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { QuestionsModule } from './questions/questions.module';
+import { AnswersModule } from './answers/answers.module';
 
 @Module({
-  imports: [QuestionsModule],
+  imports: [QuestionsModule, AnswersModule],
   controllers: [AppController],
   providers: [AppService],
 })
